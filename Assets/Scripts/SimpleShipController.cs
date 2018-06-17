20dc4fdcdaa50b07a93ed65d0f4946f7b3b10b47
Özalp Döndüren <ozalp@donduren.com>
2018-06-18 00:13:21 +0300

    Section 6 Lecture 17 Added rigidbodies and physics materials

--- Assets/Scripts/SimpleShipController.cs
+++ Assets/Scripts/SimpleShipController.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class SimpleShipController : MonoBehaviour
 {
-    //Transform transform;
+    [Range(0.25f,25)]
+    public float thrustMultiplier = 2;
+    [Range(0.05f, 2)]
+    public float steerMultiplier = 0.5f;
+
+    //moved _delta here to make GC less busy
+    Vector2 _delta = Vector2.zero;
+    Vector2 _force = Vector2.zero;
+    float _torque;
+
+    Rigidbody2D _rigidbody;
 
     private void Awake()
     {
-        //transform = GetComponent<Transform>();   
-    }
-
-    void Start()
-    {
-
+        _rigidbody = GetComponent<Rigidbody2D>();   
     }
 
     void Update()
     {
-        float x = 0f;
-        float y = 0f;
-        float speed = 0.5f;
 #if (UNITY_IOS || UNITY_ANDROID) && (REMOTE || !UNITY_EDITOR)
         if (Input.touchCount > 0)
         {
@@ -28,14 +30,21 @@ public class SimpleShipController : MonoBehaviour
             speed = 0.25f;
             if (touch.phase == TouchPhase.Moved)
             {
-                x = touch.deltaPosition.x * speed;
-                y = touch.deltaPosition.y * speed;
+                delta = touch.deltaPosition;
             }
         }
 #else
-        x = Input.GetAxis("Horizontal") * speed;
-        y = Input.GetAxis("Vertical") * speed;
+        _delta.x = Input.GetAxis("Horizontal");
+        _delta.y = Input.GetAxis("Vertical");
 #endif
-        transform.Translate(x, y, 0f);
+        //transform.Translate(delta.x, delta.y, 0f);
+        //driving ship with above control become impossible
+        //after adding rigidbody to ship and asteroids 
+
+        _force.y = _delta.y * thrustMultiplier;
+        _torque = -_delta.x * steerMultiplier;
+
+        _rigidbody.AddRelativeForce(_force);
+        _rigidbody.AddTorque(_torque);
     }
 }
