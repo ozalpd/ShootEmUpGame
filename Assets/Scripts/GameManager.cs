d84ea742666c0cea7b9510b116102b02b2c3fd50
Özalp Döndüren <ozalp@donduren.com>
2018-06-21 23:25:21 +0300

    S07 E21 Added ShipDamage + (a simple version of) GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class GameManager
+{
+    public static float Damage
+    {
+        get { return _damage; }
+        set
+        {
+            if (_damage != value)
+            {
+                _damage = value;
+                Debug.Log(_damage);
+            }
+        }
+    }
+    static float _damage;
+}
---
+++ Assets/Scripts/ShipDamage.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class ShipDamage : MonoBehaviour
+{
+    Collider2D _collider;
+    Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public float Damage
+    {
+        get { return GameManager.Damage; }
+        set { GameManager.Damage = value; }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        float damage = collision.relativeVelocity.magnitude;
+        if (collision.collider.sharedMaterial != null)
+        {
+            var sharedMaterial = collision.collider.sharedMaterial;
+            damage *= _collider.sharedMaterial.friction
+                               * sharedMaterial.friction
+                               * (1 / sharedMaterial.bounciness)
+                               * (1 / _collider.sharedMaterial.friction);
+        }
+
+        if (collision.rigidbody != null)
+        {
+            damage *= collision.rigidbody.mass / _rigidbody.mass;
+        }
+
+        Damage += damage;
+    }
+}
