726e1599aabab02c1f813c985f59cd1c90c5e67d
Özalp Döndüren <ozalp@donduren.com>
2018-07-08 10:59:00 +0300

    Added new class FollowReference which follows the reference transform

---
+++ Assets/Scripts/FollowReference.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Follows the reference transform
+/// </summary>
+public class FollowReference : MonoBehaviour
+{
+    public Transform reference;
+
+    public float speed = 1f;
+    [Tooltip("Angular rotation speed. If this value is zero, has no effect")]
+    public float rotationSpeed;
+
+    void Update()
+    {
+        if (rotationSpeed > 0)
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, reference.rotation, rotationSpeed * Time.deltaTime);
+
+        transform.position = Vector3.MoveTowards(transform.position, reference.position, speed * Time.deltaTime);
+    }
+}
